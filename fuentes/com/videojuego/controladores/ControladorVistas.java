18bc12667b5f17b05d2c4fe817b04b7668df13f0
dmunozc04-albarregas <dmunozc04@iesalbarregas.es>
2025-05-08 13:35:45 +0200

    Musica agregada

--- fuentes/com/videojuego/controladores/ControladorVistas.java
+++ fuentes/com/videojuego/controladores/ControladorVistas.java
@@ -7,8 +7,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.text.Font;
 import java.io.IOException;
+import java.io.File;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class ControladorVistas extends Controlador {
 
@@ -19,6 +22,16 @@ public class ControladorVistas extends Controlador {
 		this.ventana = ventana;
 		ventana.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
 	    Font.loadFont(getClass().getClassLoader().getResourceAsStream("PressStart2P.ttf"), 10);
+
+	    String pathMusica = "recursos/8_bit.mp3";
+
+	    Media media = new Media(new File(pathMusica).toURI().toString());
+
+	    //Media Player
+	    MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); // Repetir en bucle
+        mediaPlayer.play(); // Iniciar reproducción
+
 
 		vista1 = Controlador.cargarVista("/com/videojuego/vistas/pantalla_splash.fxml");
 		vista1.getStylesheets().add(getClass().getResource("/estilo.css").toExternalForm());
